6de8588f900b949e8820d9366dbd5e4e11d0f2b2
Felipe Arruda <felipesouzaero@gmail.com>
2024-07-31 22:01:33 -0300

    feat: Saving users in memory (Headers)

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,13 +1,21 @@
 import http from 'node:http'
 
+const users = []
 const server = http.createServer((request, response) => {
     const { method, url } = request;
 
     if (method === "GET" && url === '/users') {
-        return response.end('Listagem de usuários.')
+        return response
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if (method === "POST" && url === "/users") {
+        users.push({
+            id: 1,
+            name: 'Felipe',
+            email: 'felipearruda@gmail.com'
+        })
         return response.end('Criação de usuário')
     }
     return response.end('Salve!')
