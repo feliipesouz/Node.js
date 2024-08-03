01ab1f9b64b94aa4ea8cb8e7a41bc3f82d1f7b01
Felipe Arruda <felipesouzaero@gmail.com>
2024-08-03 17:55:56 -0300

    feat: Request body in JSON (Stream & Buffers)

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,8 +1,20 @@
 import http from 'node:http'
 
 const users = []
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     const { method, url } = request;
+
+    const buffers = []
+
+    for await (const chunk of request) {
+        buffers.push(chunk)
+    }
+
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch (error) {
+        request.body = null
+    }
 
     if (method === "GET" && url === '/users') {
         return response
@@ -11,10 +23,12 @@ const server = http.createServer((request, response) => {
     }
 
     if (method === "POST" && url === "/users") {
+        const { name, email } = request.body
+
         users.push({
             id: 1,
-            name: 'Felipe',
-            email: 'felipearruda@gmail.com'
+            name,
+            email
         })
         return response.writeHead(201).end('Criação de usuário')
     }
