8f786b0a86c5ad40a2c47694b2a9565f78c66ffd
Felipe Arruda <felipesouzaero@gmail.com>
2024-08-13 21:58:02 -0300

    feat: Automated Tests - Categorizing Tests

--- aulas/02-API-REST-NODEJS/test/example.spec.ts
+++ aulas/02-API-REST-NODEJS/test/example.spec.ts
@@ -1,24 +1,26 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, describe, test } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
 
-beforeAll(async () => {
-    await app.ready()
-})
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
 
-afterAll(async () => {
-    await app.close()
-})
+    afterAll(async () => {
+        await app.close()
+    })
 
-test('O usuário consegue criar uma nova transação', async () => {
-    await request(app.server)
-        .post('/transactions')
-        .send({
-            title: "New transaction",
-            amount: 5000,
-            type: 'credit'
-        })
-        .expect(201)
+    test('O usuário consegue criar uma nova transação', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: "New transaction",
+                amount: 5000,
+                type: 'credit'
+            })
+            .expect(201)
+    })
 })
 
