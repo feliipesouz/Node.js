e33a066810868b1f2dd2eb68abcfc2667974d4aa
Felipe Arruda <felipesouzaero@gmail.com>
2024-08-24 19:21:39 -0300

    feat: Integration with Prisma ORM - Prisma ORM Fundamentals

--- aulas/03-api-solid/src/app.ts
+++ aulas/03-api-solid/src/app.ts
@@ -1,3 +1,15 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-export const app = fastify()
+export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Felipe Arruda",
+    email: "felipesouzaero@gmail.com",
+  },
+});
+
+// ORM - Object Relational Mapper
