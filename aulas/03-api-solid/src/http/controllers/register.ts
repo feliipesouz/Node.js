f750fa43c29873d5dae0fe6ea075a78aeb76ab39
Felipe Arruda <felipesouzaero@gmail.com>
2024-08-25 16:06:02 -0300

    feat: Use Case and Design Patterns - Dealing with use case errors

--- aulas/03-api-solid/src/http/controllers/register.ts
+++ aulas/03-api-solid/src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -12,14 +13,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const prismaUsersRepository = new PrismaUsersRepository();
-  const registerUseCase = new RegisterUseCase(prismaUsersRepository);
-
   try {
-    await registerUseCase.execute({ name, email, password });
-    return reply.status(201).send();
-  } catch (error) {
-    console.error(error);
-    return reply.status(409).send();
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message });
+    }
+    return reply.status(500).send();
   }
+  return reply.status(201).send();
 }
