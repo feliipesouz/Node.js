d61de46187e87334d7f0daae58f355f15fc893be
Felipe Arruda <felipesouzaero@gmail.com>
2024-08-26 10:44:38 -0300

    feat: Design Patterns & Tests - First unit test

--- aulas/03-api-solid/src/use-cases/register.spec.ts
+++ aulas/03-api-solid/src/use-cases/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from "vitest";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { describe, expect, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
 
-test("Check if it works", () => {
-  expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+  it("Should hash user password upon registration", async () => {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+    console.log("oi");
+    const { user } = await registerUseCase.execute({
+      name: "Felipe Arruda",
+      email: "felipesoauzaero2aaaa1@gmail.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
