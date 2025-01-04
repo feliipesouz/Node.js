a992e66d4edf30a953fb1309c95bb523c29c8dd4
Felipe Arruda <felipesouzaero@gmail.com>
2025-01-04 19:29:42 -0300

    feat: E2E Controllers & Tests - Prisma Test Environment

--- aulas/03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ aulas/03-api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest/environments";
 
-export default <Environment> {
-    name: 'prisma',
-    transformMode: 'ssr',
-    async setup(){
-        console.log('Setup')
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
 
-        return {
-            teardown(){
-                console.log('Teardown')
-            }
-        }
-    }
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
 }
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "ssr",
+  async setup() {
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
+      },
+    };
+  },
+};
