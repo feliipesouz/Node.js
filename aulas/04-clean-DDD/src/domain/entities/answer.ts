fe4be92340bb035bc0bc1c9d294b3775e976d66d
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-01-17 14:52:49 -0300

    feat: Fundamentos de DDD - Mapeando propriedades

--- aulas/04-clean-DDD/src/domain/entities/answer.ts
+++ aulas/04-clean-DDD/src/domain/entities/answer.ts
@@ -1,13 +1,16 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface AnswerProps {
+  questionId: UniqueEntityID;
+  authorId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
-  get content(){
+  get content() {
     return this.props.content
   }
 }
