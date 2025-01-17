8e2096a20dab5363a675cbe108de1406ad6ff6e3
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-01-17 14:36:04 -0300

    feat: Fundamentos de DDD - ID das entidades

--- aulas/04-clean-DDD/src/core/entities/entity.ts
+++ aulas/04-clean-DDD/src/core/entities/entity.ts
@@ -1,15 +1,16 @@
 import { randomUUID } from "crypto";
+import { UniqueEntityID } from "./unique-entity-id";
 
-export class Entity<T> {
-  private _id: string;
-  protected props: any;
+export class Entity<Props> {
+  private _id: UniqueEntityID;
+  protected props: Props;
 
   get id() {
     return this._id;
   }
 
-  constructor(props: any, id?: string) {
+  constructor(props: Props, id?: string) {
     this.props = props;
-    this._id = id ?? randomUUID();
+    this._id = new UniqueEntityID(id)
   }
 }
