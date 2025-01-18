5cc73b41b780a8dec7f442e98f6e2e8c56ee739b
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-01-18 19:55:45 -0300

    feat: Fórum com DDD & Clean Architecture - Caso de uso: Criar pergunta

--- aulas/04-clean-DDD/src/domain/forum/application/use-cases/answer-question.spec.ts
+++ aulas/04-clean-DDD/src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,22 +1,22 @@
-import { expect, test } from 'vitest'
-import { AnswerQuestionUseCase } from './answer-question'
-import { AnswerRepository } from '../../../repositories/answer-repository'
-import { Answer } from '../../enterprise/entities/answer'
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../../enterprise/entities/answer";
 
 const fakeAnswersRepository: AnswerRepository = {
   create: async (answer: Answer) => {
-    console.log(answer)
+    console.log(answer);
   },
-}
+};
 
-test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
 
   const answer = await answerQuestion.execute({
-    questionId: '1',
-    instructorId: '1',
-    content: 'Nova resposta',
-  })
+    questionId: "1",
+    instructorId: "1",
+    content: "Nova resposta",
+  });
 
-  expect(answer.content).toEqual('Nova resposta')
-})
+  expect(answer.content).toEqual("Nova resposta");
+});
