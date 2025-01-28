6357310d624084996be9407371685ee7d9699628
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-01-27 21:36:06 -0300

    feat: Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da resposta

--- aulas/04-clean-DDD/src/domain/forum/application/repositories/answer-comments-repository.ts
+++ aulas/04-clean-DDD/src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,10 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerComment } from "../../enterprise/entities/answer-comment";
+import { QuestionComment } from "../../enterprise/entities/question-comment";
 
 export interface AnswerCommentsRepository {
     findById(id: string): Promise<AnswerComment | null>
+    findManyByAnswerId(answerId: string, params: PaginationParams): Promise<AnswerComment[]>
     create(answerComment: AnswerComment): Promise<void> 
     delete(answerComment: AnswerComment): Promise<void>
 }
