cc0f2f1f50202e1f2c50ed669953e650ae319ae4
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-01-29 18:33:51 -0300

    feat: Functional Error Handling - Functional Error Handling

--- aulas/04-clean-DDD/src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ aulas/04-clean-DDD/src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { Either, left, right } from "@/core/either";
 import { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
 
 
@@ -6,9 +7,7 @@ interface DeleteAnswerCommentUseCaseRequest {
     authorId: string,
 }
 
-interface DeleteAnswerCommentUseCaseResponse {
-
-}
+type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
 
 export class DeleteAnswerCommentUseCase {
     constructor(
@@ -18,16 +17,15 @@ export class DeleteAnswerCommentUseCase {
         const answerComment = await this.answerCommentsRepository.findById(answerCommentId)
 
         if(!answerComment) {
-            throw new Error('Answer comment not found.')
+            return left('Answer comment not found.')
         }
 
         if(answerComment.authorId.toString() !== authorId){
-            throw new Error('Not allowed.')
+           return left('Not allowed.')
         }
 
         await this.answerCommentsRepository.delete(answerComment)
 
-        return {}
-
+        return right({})
     }
 }
