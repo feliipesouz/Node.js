6b1db3564107c8636d4c4e62fcc66dfb7bb4f394
FELIPE ARRUDA DE SOUZA <felipearruda@3coracoes.com.br>
2025-02-04 20:38:33 -0300

    feat: Aggregates & Watched Lists - Excluindo anexos da pergunta

--- aulas/04-clean-DDD/test/repositories/in-memory-question-attachments-repository.ts
+++ aulas/04-clean-DDD/test/repositories/in-memory-question-attachments-repository.ts
@@ -3,11 +3,18 @@ import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-
 
 export class InMemoryQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
     public items: QuestionAttachment[] = [];
-
+    
     async findManyByQuestionId(questionId: string) {
         const questionAttachments = this.items
         .filter((item) => item.questionId.toString() === questionId)
-
+        
         return questionAttachments 
     }    
+
+    async deleteManyByQuestionId(questionId: string): Promise<void> {
+        const questionAttachments = this.items
+        .filter((item) => item.questionId.toString() !== questionId)
+
+        this.items = questionAttachments
+    }
 }
