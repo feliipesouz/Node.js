76e369568a2c0825d7c1507079f2ccc4476f64f4
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-04-19 20:13:25 -0300

    feat: Criando serviço do Prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleDestroy, OnModuleInit {
+    public client: PrismaClient
+
+    constructor() {
+        super({
+            log: ['warn', 'error']
+        })
+    }
+    onModuleInit() {
+        return this.$connect()
+    }
+    onModuleDestroy() {
+        return this.$disconnect()
+    }
+}
