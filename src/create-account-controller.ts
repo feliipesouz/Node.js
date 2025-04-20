9198eedf356e7d6033d9791d31375a645bd8d222
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-04-20 11:21:38 -0300

    feat: Extensão Rest Client no VSCode

--- src/create-account-controller.ts
+++ src/create-account-controller.ts
@@ -4,7 +4,6 @@ import { hash } from 'bcryptjs'
 import { z } from "zod";
 import { ZodValidationPipe } from "./pipes/zod-validation-pipe";
 
-
 const createAccountBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,4 +1,3 @@
-
 import { PipeTransform, BadRequestException } from '@nestjs/common';
 import { ZodError, ZodSchema } from 'zod';
 import { fromZodError } from 'zod-validation-error'
