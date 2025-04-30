11430b61029fa92d0757d5bba62a80088ffd9950
Felipe Arruda <felipearruda@MacBook-Air-de-Felipe.local>
2025-04-29 22:01:16 -0300

    feat: Protegendo rotas com Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './create-account-controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { JwtStrategy } from './auth/jwt.strategy'
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate: (env) => envSchema.parse(env),
     isGlobal: true
   })],
-  controllers: [CreateAccountController, AuthenticateController],
-  providers: [PrismaService],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+  providers: [PrismaService, JwtStrategy],
 })
 export class AppModule { }
